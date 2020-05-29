aa1fda27a4703a1a9eaeb5b42e320c150183c767
Rajeevgl <60258084+Rajeevgl@users.noreply.github.com>
2020-05-29 23:33:37 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Titles from './Components/Title'
 import Synonym from './Components/Synonym'
 import Form from './Components/Form'
+import FormP from './Components/FormP';
+import Plural from './Components/Plural';
 
 class App extends Component{
   state = {
@@ -16,45 +18,52 @@ class App extends Component{
     if(word){
       const api_call = await fetch(`http://localhost:8080/synonym/${word}`)
       const data = await api_call.json()
-      //console.log(word);
-      // if((data.cod = "404")){
-      //   this.setState({
-      //     word : undefined,
-      //     synonym : undefined
-      //   });
-      // }
-      // else{
-      //   console.log(word);
-      //   this.setState({
-      //     word : word,
-      //     synonym : data.array,
-      //   });
-      // }
+      const str = JSON.stringify(data).split('"')
+      // const len = str.length - 1;
       this.setState({
         word : word,
-        synonym : JSON.stringify(data)
+        synonym : str.slice(1,10)
+      });
+      console.log(word);
+      console.log(typeof data);
+    }
+  }
+  getPlural = async(e) => {
+    const word = e.target.elements.Word.value
+    e.preventDefault();
+    if(word){
+      const api_call = await fetch(`http://localhost:8080/plural/${word}`)
+      const data = await api_call.json()
+      this.setState({
+        word : word,
+        plural : data.plural
       });
       console.log(word);
       console.log(typeof data);
     }
   }
 
-  render(){
+    render(){
     return(
       <div>
         <div className = "wrapper">
           <div className = "main">
             <div className = "container">
               <div className = "row">
-                <div className = "col-xs-5 title-container">
+                <div className = "title-container">
                   <Titles />
-                </div>
-                <div className = "col-xs-7 form-container">
-                  <Form getSynonym = {this.getSynonym} />
-                  <Synonym
-                    word = {this.state.word}
-                    synonym = {this.state.synonym}
-                  /> 
+                  <div className = "form-container">
+                    <Form getSynonym = {this.getSynonym} />
+                    <Synonym
+                      word = {this.state.word}
+                      synonym = {this.state.synonym}
+                    />
+                    <FormP getPlural = {this.getPlural} />
+                    <Plural
+                      word = {this.state.word}
+                      plural = {this.state.plural}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
